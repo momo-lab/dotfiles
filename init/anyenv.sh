5778f570bd6bba6cccc48b50a6d077b6bad5907d
momotaro <momotaro.n@gmail.com>
2016-05-26 20:36:22 +0900

    rbenvをanyenv経由で使用するように修正

---
+++ init/anyenv.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Initialize anyenv"
+if [[ ! -d ~/.anyenv/bin ]]; then
+  anyenv_root=${HOME}/.anyenv
+  git clone https://github.com/riywo/anyenv "${anyenv_root}"
+  git clone https://github.com/momo-lab/anyenv-plugin.git "${anyenv_root}/plugins/anyenv-plugin"
+  export PATH="${anyenv_root}/bin:$PATH"
+  eval "$(anyenv init -)"
+
+  # anyenv plugins
+  anyenv plugin-install znz/anyenv-git
+  anyenv plugin-install znz/anyenv-update
+
+  # rbenv
+  anyenv install rbenv
+  anyenv plugin-install jf/rbenv-gemset
+  anyenv plugin-install momo-lab/rbenv-install-latest
+fi
