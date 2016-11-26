d1a26e1afc7042ba5deee70a38612987cb529c46
momotaro <momotaro.n@gmail.com>
2016-11-26 20:42:56 +0900

    [install] Fix not to use vim-plug with install script

--- etc/init/vim.sh
+++ etc/init/vim.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
 echo "Initialize vim plugins."
-mkdir -p ~/.vim/autoload
-curl -fLo ~/.vim/autoload/plug.vim \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-vim +PlugInstall +qall
+vim +qall
 
