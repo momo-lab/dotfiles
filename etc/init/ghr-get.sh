ee47c5660d0954aa7c00ad23564041cc117dadb0
momotaro <momotaro.n@gmail.com>
2020-05-15 22:48:04 +0900

    [shell] ghr-get install コマンドを使用するように修正

--- etc/init/ghr-get.sh
+++ etc/init/ghr-get.sh
@@ -1,12 +1,14 @@
+#!/usr/bin/env bash
+
 root=$HOME/.ghr-get
 if [ ! -d $root ]; then
   git clone https://github.com/momo-lab/ghr-get.git $root
 fi
 
-ghr-get junegunn/fzf-bin
-ghr-get monochromegane/the_platinum_searcher
-ghr-get github/hub
-ghr-get x-motemen/ghq
-ghr-get momo-lab/git-now
-ghr-get b4b4r07/ssh-keyreg
-ghr-get stedolan/jq
+ghr-get install junegunn/fzf-bin
+ghr-get install monochromegane/the_platinum_searcher
+ghr-get install github/hub
+ghr-get install x-motemen/ghq
+ghr-get install momo-lab/git-now
+ghr-get install b4b4r07/ssh-keyreg
+ghr-get install stedolan/jq
