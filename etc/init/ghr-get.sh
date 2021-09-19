3733b926dde1facf487c61aa35ee792516fe4e20
momotaro <momotaro.n@gmail.com>
2021-09-20 07:48:47 +0900

    zsh-autosuggestionsをインストール対象から削除

--- etc/init/ghr-get.sh
+++ etc/init/ghr-get.sh
@@ -27,7 +27,7 @@ ghr-get install momo-lab/bash-abbrev-alias
 ghr-get install zsh-users/zsh-completions
 ghr-get install zsh-users/zsh-history-substring-search
 ghr-get install zsh-users/zsh-syntax-highlighting
-ghr-get install zsh-users/zsh-autosuggestions
+#ghr-get install zsh-users/zsh-autosuggestions
 ghr-get install momo-lab/zsh-abbrev-alias
 ghr-get install momo-lab/zsh-replace-multiple-dots
 ghr-get install b4b4r07/enhancd
