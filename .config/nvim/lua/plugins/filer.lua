baa80e7bfdba3b75d277589bdfcff8785c548a19
momotaro <momotaro.n@gmail.com>
2025-01-02 15:00:27 +0900

    telescope-all-recent.nvimを導入

--- .config/nvim/lua/plugins/filer.lua
+++ .config/nvim/lua/plugins/filer.lua
@@ -122,4 +122,15 @@ return {
       },
     },
   },
+  -- 表示順を履歴から判断する
+  {
+    "prochri/telescope-all-recent.nvim",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "kkharji/sqlite.lua",
+      -- optional, if using telescope for vim.ui.select
+      --"stevearc/dressing.nvim",
+    },
+    opts = {},
+  },
 }
