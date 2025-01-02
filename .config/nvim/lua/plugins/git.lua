2e37f391753c52436eeb00bbcc87519ef5ab3866
momotaro <momotaro.n@gmail.com>
2025-01-02 14:20:01 +0900

    lazygitを導入

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,4 +1,25 @@
 return {
+  -- Git操作
+  {
+    "kdheepak/lazygit.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    keys = {
+      { "<leader>hg", "<cmd>LazyGit<CR>", desc = "LazyGit" },
+    },
+    config = function()
+      require("telescope").load_extension("lazygit")
+    end,
+  },
   -- Gitの変更を表示とStagingなどの操作
   {
     "lewis6991/gitsigns.nvim",
